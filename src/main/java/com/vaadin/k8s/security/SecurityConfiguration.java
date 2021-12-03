1cf339dde5355d02b989a763381531d00b54e9cf
Manolo Carrasco <manolo@vaadin.com>
2021-12-03 14:14:44 +0200

    feat: support stateless authentication tokens

--- src/main/java/com/vaadin/k8s/security/SecurityConfiguration.java
+++ src/main/java/com/vaadin/k8s/security/SecurityConfiguration.java
@@ -1,7 +1,13 @@
 package com.vaadin.k8s.security;
 
-import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+import java.util.Base64;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+import com.vaadin.k8s.views.login.LoginView;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,13 +15,16 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import com.vaadin.k8s.views.login.LoginView;
+import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 
     public static final String LOGOUT_URL = "/";
+
+    @Value("${jwt.auth.secret}")
+    private String authSecret;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -24,9 +33,12 @@ public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         super.configure(http);
         setLoginView(http, LoginView.class, LOGOUT_URL);
+        setStatelessAuthentication(http,
+                new SecretKeySpec(Base64.getDecoder().decode(authSecret),
+                        JwsAlgorithms.HS256),
+                "com.vaadin.k8s.demo", 3600);
     }
 
     @Override
