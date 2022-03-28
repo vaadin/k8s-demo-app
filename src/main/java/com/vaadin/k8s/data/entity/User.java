56ca23cd840f6b8f046afc714fe1fc348344b109
Manolo Carrasco <manolo@vaadin.com>
2022-03-28 12:38:03 +0200

    fix: user is a reserved word, renaming table

--- src/main/java/com/vaadin/k8s/data/entity/User.java
+++ src/main/java/com/vaadin/k8s/data/entity/User.java
@@ -7,12 +7,14 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Lob;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.vaadin.k8s.data.AbstractEntity;
 import com.vaadin.k8s.data.Role;
 
 @Entity
+@Table(name = "ApplicationUser")
 public class User extends AbstractEntity {
 
     @Column(unique = true)
@@ -28,30 +30,39 @@ public class User extends AbstractEntity {
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getHashedPassword() {
         return hashedPassword;
     }
+
     public void setHashedPassword(String hashedPassword) {
         this.hashedPassword = hashedPassword;
     }
+
     public Set<Role> getRoles() {
         return roles;
     }
+
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
     }
+
     public String getProfilePictureUrl() {
         return profilePictureUrl;
     }
+
     public void setProfilePictureUrl(String profilePictureUrl) {
         this.profilePictureUrl = profilePictureUrl;
     }
