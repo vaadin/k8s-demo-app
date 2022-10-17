d2116a5254abc88db3cda79e6c0752eb79b64154
Manolo Carrasco <manolo@vaadin.com>
2022-10-17 07:53:41 +0200

    add option <all> to the build script

--- build-app.sh
+++ build-app.sh
@@ -11,34 +11,38 @@ APP=k8s-demo-app
 VERS=$1
 CSSFILE=frontend/themes/k8s-demo-app/styles.css
 
-# Compute an set colors per each version
+deploy() {
+  echo ""
+  echo ">> Changing colors for ${VERS} ($base, $primary) ..."
+  sed -i~ \
+    -e 's/--lumo-base-color:.*;/--lumo-base-color: '$base';/g' \
+    -e 's/--lumo-primary-color:.*;/--lumo-primary-color: '$primary';/g' \
+    ${CSSFILE}
+
+  # Package maven app
+  echo ""
+  echo ">> Packaging Maven Application ${APP} ..."
+  mvn package -Pproduction -B -q
+
+  # Restore back modified colors
+  [ -f ${CSSFILE}~ ] && mv ${CSSFILE}~ ${CSSFILE}
+
+  # Build docker image
+  echo ""
+  echo ">> Building Docker Image ${USER}/${APP}:${VERS} ..."
+  docker build --quiet --tag ${USER}/${APP}:${VERS} .
+
+  # Publish docker image
+  echo ""
+  echo ">> Publishing Image in Docker Hub (You need privs to push to ${USER}/${APP}) ..."
+  docker push --quiet ${USER}/${APP}:${VERS}
+}
+
+# Compute and set colors per each version
 case ${VERS} in
-  1.0) base="#233348"; primary="#2a7fef";;
-  2.0) base="#2c6d3b"; primary="#194e19";;
-  3.0) base="#926513"; primary="#c2881f";;
-  *) echo "Usage $0 <1.0|2.0|3.0>" && exit 1;;
+  1.0) base="#233348" primary="#2a7fef" deploy;;
+  2.0) base="#2c6d3b" primary="#194e19" deploy;;
+  3.0) base="#926513" primary="#c2881f" deploy;;
+  all) sh $0 1.0 && sh $0 2.0 && sh $0 3.0;;
+  *) echo "Usage $0 <1.0|2.0|3.0|all>" && exit 1;;
 esac
-echo ""
-echo ">> Changing colors for ${VERS} ($base, $primary) ..."
-sed -i~ \
-  -e 's/--lumo-base-color:.*;/--lumo-base-color: '$base';/g' \
-  -e 's/--lumo-primary-color:.*;/--lumo-primary-color: '$primary';/g' \
-  ${CSSFILE}
-
-# Package maven app
-echo ""
-echo ">> Packaging Maven Application ${APP} ..."
-mvn package -Pproduction -B -q
-
-# Restore back modified colors
-[ -f ${CSSFILE}~ ] && mv ${CSSFILE}~ ${CSSFILE}
-
-# Build docker image
-echo ""
-echo ">> Building Docker Image ${USER}/${APP}:${VERS} (You need privs to push to ${USER}/${APP})..."
-docker build --tag ${USER}/${APP}:${VERS} .
-
-# Publish docker image
-echo ""
-echo ">> Publishing Image in Docker Hub (You need privs to push to ${USER}/${APP}) ..."
-docker push ${USER}/${APP}:${VERS}
