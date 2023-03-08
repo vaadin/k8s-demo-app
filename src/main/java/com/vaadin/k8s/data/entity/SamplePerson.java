31e2d1eb4b75923c536e7b135a7d70c5446ba262
Manolo Carrasco <manolo@vaadin.com>
2023-03-08 19:14:57 +0100

    chore: update to update-to-v24

--- src/main/java/com/vaadin/k8s/data/entity/SamplePerson.java
+++ src/main/java/com/vaadin/k8s/data/entity/SamplePerson.java
@@ -1,11 +1,11 @@
 package com.vaadin.k8s.data.entity;
 
-import javax.persistence.Entity;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import com.vaadin.k8s.data.AbstractEntity;
 import java.time.LocalDate;
