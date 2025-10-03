a24986e06608aaaa58099afb54bab3e7f18bf109
Marco Collovati <mcollovati@gmail.com>
2025-10-03 13:49:23 +0200

    chore: replace VaadinWebSecurity with VaadinSecurityConfigurer (#147)

--- src/main/java/com/vaadin/k8s/security/SecurityConfiguration.java
+++ src/main/java/com/vaadin/k8s/security/SecurityConfiguration.java
@@ -1,25 +1,30 @@
 package com.vaadin.k8s.security;
 
-import java.util.Base64;
-
-import javax.crypto.spec.SecretKeySpec;
-
+import com.vaadin.flow.spring.security.VaadinAwareSecurityContextHolderStrategyConfiguration;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import com.vaadin.flow.spring.security.stateless.VaadinStatelessSecurityConfigurer;
 import com.vaadin.k8s.views.login.LoginView;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+import static com.vaadin.flow.spring.security.VaadinSecurityConfigurer.vaadin;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfiguration extends VaadinWebSecurity {
+@Import(VaadinAwareSecurityContextHolderStrategyConfiguration.class)
+public class SecurityConfiguration {
 
     public static final String LOGOUT_URL = "/";
 
@@ -31,19 +36,15 @@ public class SecurityConfiguration extends VaadinWebSecurity {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        setLoginView(http, LoginView.class, LOGOUT_URL);
-        setStatelessAuthentication(http,
-                new SecretKeySpec(Base64.getDecoder().decode(authSecret),
-                        JwsAlgorithms.HS256),
-                "com.vaadin.k8s.demo", 3600);
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
-        web.ignoring().requestMatchers("/images/*.png");
+    @Bean
+    public SecurityFilterChain vaadinSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/images/*.png").permitAll());
+        http.with(vaadin(), vaadin -> vaadin.loginView(LoginView.class, LOGOUT_URL));
+        http.with(new VaadinStatelessSecurityConfigurer<>(), cfg ->
+                cfg.issuer("com.vaadin.k8s.demo").expiresIn(3600)
+                        .withSecretKey(key -> key.secretKey(
+                                new SecretKeySpec(Base64.getDecoder().decode(authSecret), JwsAlgorithms.HS256)
+                        )));
+        return http.build();
     }
 }
